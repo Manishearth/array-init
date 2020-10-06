74866d0eafcf62e083ff7827e07dbdfa886ad068
Arthur Carcano <53921575+krtab@users.noreply.github.com>
2020-10-06 14:16:13 +0200

    Cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -43,11 +43,8 @@
 //! ```
 
 use ::core::{
-    mem::{self,
-        MaybeUninit,
-    },
-    ptr,
-    slice,
+    mem::{self, MaybeUninit},
+    ptr, slice,
 };
 
 /// Trait for things which are actually arrays.
@@ -84,21 +81,20 @@ pub unsafe trait IsArray {
 ///
 /// assert!(arr.iter().enumerate().all(|(i, &x)| x == i * i));
 /// ```
-pub fn array_init<Array, F> (
-    mut initializer: F,
-) -> Array
+pub fn array_init<Array, F>(mut initializer: F) -> Array
 where
-    Array : IsArray,
-    F : FnMut(usize) -> Array::Item,
+    Array: IsArray,
+    F: FnMut(usize) -> Array::Item,
 {
     enum Unreachable {}
 
-    try_array_init( // monomorphise into an unfallible version
-        move |i| -> Result<Array::Item, Unreachable> {
-            Ok(initializer(i))
-        }
-    ).unwrap_or_else( // zero-cost unwrap
-        |unreachable| match unreachable { /* ! */ }
+    try_array_init(
+        // monomorphise into an unfallible version
+        move |i| -> Result<Array::Item, Unreachable> { Ok(initializer(i)) },
+    )
+    .unwrap_or_else(
+        // zero-cost unwrap
+        |unreachable| match unreachable { /* ! */ },
     )
 }
 
@@ -124,28 +120,23 @@ where
 /// let mut iter = four.iter().copied().cycle();
 /// let arr: [u32; 50] = array_init::from_iter(iter).unwrap();
 /// ```
-pub fn from_iter<Array, Iterable> (
-    iterable: Iterable,
-) -> Option<Array>
+pub fn from_iter<Array, Iterable>(iterable: Iterable) -> Option<Array>
 where
-    Iterable : IntoIterator<Item = Array::Item>,
-    Array : IsArray,
+    Iterable: IntoIterator<Item = Array::Item>,
+    Array: IsArray,
 {
     try_array_init({
         let mut iterator = iterable.into_iter();
-        move |_| {
-            iterator.next().ok_or(())
-        }
-    }).ok()
+        move |_| iterator.next().ok_or(())
+    })
+    .ok()
 }
 
 #[inline]
-pub fn try_array_init<Array, Err, F> (
-    mut initializer: F,
-) -> Result<Array, Err>
+pub fn try_array_init<Array, Err, F>(mut initializer: F) -> Result<Array, Err>
 where
-    Array : IsArray,
-    F : FnMut(usize) -> Result<Array::Item, Err>,
+    Array: IsArray,
+    F: FnMut(usize) -> Result<Array::Item, Err>,
 {
     if !mem::needs_drop::<Array::Item>() {
         let mut array: MaybeUninit<Array> = MaybeUninit::uninit();
@@ -160,7 +151,7 @@ where
         //   - `IsArray`'s contract guarantees that we are within the array
         //     since we have `0 <= i < Array::len`
         unsafe {
-            for i in 0 .. Array::len() {
+            for i in 0..Array::len() {
                 let value_i = initializer(i)?;
                 ptr_i.write(value_i);
                 ptr_i = ptr_i.add(1);
@@ -183,18 +174,15 @@ where
     }
 
     impl<Item> Drop for UnsafeDropSliceGuard<Item> {
-        fn drop (self: &'_ mut Self)
-        {
+        fn drop(self: &'_ mut Self) {
             unsafe {
                 // # Safety
                 //
                 //   - the contract of the struct guarantees that this is sound
-                ptr::drop_in_place(
-                    slice::from_raw_parts_mut(
-                        self.base_ptr,
-                        self.initialized_count,
-                    )
-                );
+                ptr::drop_in_place(slice::from_raw_parts_mut(
+                    self.base_ptr,
+                    self.initialized_count,
+                ));
             }
         }
     }
@@ -221,7 +209,7 @@ where
             initialized_count: 0,
         };
 
-        for i in 0 .. Array::len() {
+        for i in 0..Array::len() {
             // Invariant: `i` elements have already been initialized
             panic_guard.initialized_count = i;
             // If this panics or fails, `panic_guard` is dropped, thus
@@ -315,116 +303,116 @@ mod tests {
     use super::*;
 
     #[test]
-    fn seq ()
-    {
+    fn seq() {
         let seq: [usize; 5] = array_init(|i| i);
         assert_eq!(&[0, 1, 2, 3, 4], &seq);
     }
 
     #[test]
-    fn array_from_iter ()
-    {
+    fn array_from_iter() {
         let array = [0, 1, 2, 3, 4];
         let seq: [usize; 5] = from_iter(array.iter().copied()).unwrap();
-        assert_eq!(
-            array,
-            seq,
-        );
+        assert_eq!(array, seq,);
     }
 
     #[test]
-    fn array_init_no_drop ()
-    {
+    fn array_init_no_drop() {
         DropChecker::with(|drop_checker| {
-            let result: Result<[_; 5], ()> =
-                try_array_init(|i| {
-                    if i < 3 {
-                        Ok(drop_checker.new_element())
-                    } else {
-                        Err(())
-                    }
-                })
-            ;
+            let result: Result<[_; 5], ()> = try_array_init(|i| {
+                if i < 3 {
+                    Ok(drop_checker.new_element())
+                } else {
+                    Err(())
+                }
+            });
             assert!(result.is_err());
         });
     }
 
     #[test]
-    fn from_iter_no_drop ()
-    {
+    fn from_iter_no_drop() {
         DropChecker::with(|drop_checker| {
-            let iterator = (0 .. 3).map(|_| drop_checker.new_element());
+            let iterator = (0..3).map(|_| drop_checker.new_element());
             let result: Option<[_; 5]> = from_iter(iterator);
             assert!(result.is_none());
         });
     }
 
     #[test]
-    #[cfg(feature="const-generics")]
-    fn test_513_len(){
+    #[cfg(feature = "const-generics")]
+    fn test_513_len() {
         assert_eq!(<[u32; 513] as IsArray>::len(), 513)
     }
 
     #[test]
-    #[cfg(feature="const-generics")]
-    fn test_513_seq(){
+    #[cfg(feature = "const-generics")]
+    fn test_513_seq() {
         let seq: [usize; 513] = array_init(|i| i);
-        assert_eq!([
-0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,
-31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,
-61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,
-91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,
-121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,
-151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,
-181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,
-211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,
-241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,
-271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,300,
-301,302,303,304,305,306,307,308,309,310,311,312,313,314,315,316,317,318,319,320,321,322,323,324,325,326,327,328,329,330,
-331,332,333,334,335,336,337,338,339,340,341,342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,358,359,360,
-361,362,363,364,365,366,367,368,369,370,371,372,373,374,375,376,377,378,379,380,381,382,383,384,385,386,387,388,389,390,
-391,392,393,394,395,396,397,398,399,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,419,420,
-421,422,423,424,425,426,427,428,429,430,431,432,433,434,435,436,437,438,439,440,441,442,443,444,445,446,447,448,449,450,
-451,452,453,454,455,456,457,458,459,460,461,462,463,464,465,466,467,468,469,470,471,472,473,474,475,476,477,478,479,480,
-481,482,483,484,485,486,487,488,489,490,491,492,493,494,495,496,497,498,499,500,501,502,503,504,505,506,507,508,509,510,
-511,512
-        ], seq);
+        assert_eq!(
+            [
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+                23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
+                44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
+                65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85,
+                86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104,
+                105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
+                121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136,
+                137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152,
+                153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168,
+                169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184,
+                185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200,
+                201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216,
+                217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232,
+                233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248,
+                249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264,
+                265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280,
+                281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296,
+                297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312,
+                313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328,
+                329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344,
+                345, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360,
+                361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374, 375, 376,
+                377, 378, 379, 380, 381, 382, 383, 384, 385, 386, 387, 388, 389, 390, 391, 392,
+                393, 394, 395, 396, 397, 398, 399, 400, 401, 402, 403, 404, 405, 406, 407, 408,
+                409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422, 423, 424,
+                425, 426, 427, 428, 429, 430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440,
+                441, 442, 443, 444, 445, 446, 447, 448, 449, 450, 451, 452, 453, 454, 455, 456,
+                457, 458, 459, 460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472,
+                473, 474, 475, 476, 477, 478, 479, 480, 481, 482, 483, 484, 485, 486, 487, 488,
+                489, 490, 491, 492, 493, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504,
+                505, 506, 507, 508, 509, 510, 511, 512
+            ],
+            seq
+        );
     }
 
     use self::drop_checker::DropChecker;
     mod drop_checker {
         use ::core::cell::Cell;
 
-        pub(in super)
-        struct DropChecker {
+        pub(super) struct DropChecker {
             slots: [Cell<bool>; 512],
             next_uninit_slot: Cell<usize>,
         }
 
-        pub(in super)
-        struct Element<'drop_checker> {
+        pub(super) struct Element<'drop_checker> {
             slot: &'drop_checker Cell<bool>,
         }
 
         impl Drop for Element<'_> {
-            fn drop (self: &'_ mut Self)
-            {
+            fn drop(self: &'_ mut Self) {
                 assert!(self.slot.replace(false), "Double free!");
             }
         }
 
         impl DropChecker {
-            pub(in super)
-            fn with (f: impl FnOnce(&Self))
-            {
+            pub(super) fn with(f: impl FnOnce(&Self)) {
                 let drop_checker = Self::new();
                 f(&drop_checker);
                 drop_checker.assert_no_leaks();
             }
 
-            pub(in super)
-            fn new_element (self: &'_ Self) -> Element<'_>
-            {
+            pub(super) fn new_element(self: &'_ Self) -> Element<'_> {
                 let i = self.next_uninit_slot.get();
                 self.next_uninit_slot.set(i + 1);
                 self.slots[i].set(true);
@@ -433,22 +421,18 @@ mod tests {
                 }
             }
 
-            fn new () -> Self
-            {
+            fn new() -> Self {
                 Self {
                     slots: crate::array_init(|_| Cell::new(false)),
                     next_uninit_slot: Cell::new(0),
                 }
             }
 
-            fn assert_no_leaks (self: Self)
-            {
-                let leak_count: usize =
-                    self.slots[.. self.next_uninit_slot.get()]
-                        .iter()
-                        .map(|slot| usize::from(slot.get() as u8))
-                        .sum()
-                ;
+            fn assert_no_leaks(self: Self) {
+                let leak_count: usize = self.slots[..self.next_uninit_slot.get()]
+                    .iter()
+                    .map(|slot| usize::from(slot.get() as u8))
+                    .sum();
                 assert_eq!(leak_count, 0, "No elements leaked");
             }
         }
